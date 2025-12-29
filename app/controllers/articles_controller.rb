d02a29ccbf80856046fdaf2e9fdd092e76b95dfa
maykol68 <sm9365277@gmail.com>
2025-12-29 18:09:39 -0500

    crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,54 @@ class ArticlesController < ApplicationController
     def index
         @Articles = Article.all
     end
+
+    def show 
+      article
+    end
+
+    def new
+        @article  = Article.new
+    end
+
+    def create 
+        @article  = Article.new(article_params)
+
+        if @article.save
+            redirect_to articles_path, notice: "article created"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        article
+    end
+
+    def update
+        
+        if article.update(article_params)
+            redirect_to articles_path, notice: "article updated"
+        else
+            render :edit, status: unprocessable_entity
+        end
+    end
+
+    def destroy
+        
+        if article.delete
+            redirect_to articles_path, notice: "article deleted"
+        else
+            render :show, alert: "no se pudo eliminar el articulo"
+        end
+    end
+
+    private
+
+    def article
+        @article = Article.find(params[:id])  
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -10,5 +10,44 @@ RSpec.describe ArticlesController, type: :controller do
                 
             expect(response).to have_http_status(:ok)
         end
+
+        it 'devolver un articulo en especifico' do 
+            article = FactoryBot.create(:article)
+
+            get :show, params: {id: article.id}
+
+            expect(response).to have_http_status(:ok)
+        end
+        it 'crear un articulo' do
+            article = FactoryBot.attributes_for(:article)
+
+            expect { post :create, params: { article: article } }.to change(Article, :count).by(1) 
+            
+            expect(response).to have_http_status(:found)
+            expect(response).to redirect_to(articles_path)
+            expect(flash[:notice]).to eq("article created")
+        end
+
+        it 'actualizar un articulo' do
+              article = FactoryBot.create(:article)
+              updated_attributes = { title: "Nuevo título", description: "Nueva descripción" }
+
+                patch :update, params: { id: article, article: updated_attributes }
+
+                article.reload
+
+                expect(article.title).to eq("Nuevo título")
+                expect(article.description).to eq("Nueva descripción")
+                expect(response).to redirect_to(articles_path)
+        end
+        it 'eliminar un articulo' do 
+            article = FactoryBot.create(:article)
+
+            expect { delete :destroy, params: { id: article.id } }.to change(Article, :count).by(-1)
+
+            expect(response).to have_http_status(:found)
+            expect(response).to redirect_to(articles_path)
+            expect(flash[:notice]).to eq("article deleted")
+        end
     end
 end
